333ac6f0742e53d169e581a0b3d6c0450e6f6e5e
maxmarumbwa <maxmarumbwa@gmail.com>
2025-11-04 06:23:19 +0200

    multiple components - home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to Dojo Blog";
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <Navbar />
-
+        <Home />
       </div>
     </div>
   );
