f764da3234b6276c804c1ddeba989b55c3f9bd73
maxmarumbwa <maxmarumbwa@gmail.com>
2025-11-04 07:02:20 +0200

    click events -btn click

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Button clicked!");
+    };
     return (  
         <div className="home">
             <h2>Home Page</h2>
             <p>Welcome to the home page of the Dojo Blog!</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
