2b77922a2ae4401b4bea88a1f53b9f33e1a96d33
maxmarumbwa <maxmarumbwa@gmail.com>
2025-11-04 05:43:02 +0200

    dynamic variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to Dojo Blog";
+  const likes = 50;
   return (
     <div className="App">
-<div className="content">
-    <h1>App content</h1>
-</div>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+                <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+      </div>
     </div>
   );
 }
